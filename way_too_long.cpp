d86311a412c002c0cdc5192847ab23e547070151
Jubayer Rahman <jrahmanraj215@gmail.com>
2024-12-02 22:29:36 +0600

    Update way_too_long.cpp

--- way_too_long.cpp
+++ way_too_long.cpp
@@ -9,13 +9,18 @@ int main(){
     {
         string str;
         cin>>str;
-        int X=str.length()-1;
+        int X=str.length();
+        if(X>10){
         auto first=str.begin()+1;
-        auto last =str.begin()+X;
+        auto last =str.begin()+X-1;
         str.erase(first,last);
         string F=to_string(X);
-       string Xu=str[0]+to_string(X-1)+str[X];
+       string Xu=str[0]+to_string(X-2)+str[X-1];    
         cout<<Xu<<endl;
+        }
+        else{
+            court<<str<<endl;
+        }
         
         n--;
         
